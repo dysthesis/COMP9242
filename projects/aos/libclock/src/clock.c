5265035ce8244ca256197893b8238c215356c041
Dysthesis <antheoraviel@protonmail.com>
2025-10-01 13:42:14 +1000

    Register and remove timers to the CPU clock and keep track of them in the queue

--- projects/aos/libclock/src/clock.c
+++ projects/aos/libclock/src/clock.c
@@ -34,6 +34,7 @@ typedef struct {
   timer_callback_t callback; // what should this timout trigger when it expires?
   size_t pos; // comforms to the pqueue_t struct, set by the pqueue functions
               // automatically
+  void *data;
 } timeout_t;
 
 static struct {
@@ -82,10 +83,154 @@ int start_timer(unsigned char *timer_vaddr) {
 }
 
 uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data) {
-  return 0;
+  if (!clock.timer_running) {
+    printf("[register_timer]: timer not running\n");
+    return CLOCK_R_UINT;
+  }
+
+  // program the hardware timer to trigger an interrupt after the delay
+
+  // we use timerA
+
+  // delay is in microseconds
+
+  // get the timestamp of the future where the callback needs to be called from
+  // delay future timestamp = current timestamp + delay
+  uint64_t current_timestamp = get_time();
+  uint64_t timeout_timestamp = current_timestamp + delay;
+
+  // check for overflow
+  if (timeout_timestamp < current_timestamp) {
+    printf("[register_timer]: overflow\n");
+    return 0; // too large to handle
+  }
+
+  // set the timer to trigger an interrupt after the delay
+  // delay is in microseconds and it is a 64 bit value
+  // all the timers are 16 bit, which are fucking useless and
+  // we have to break them into several timer interrupts
+
+  // i wont do a sophisticated timebase selection algorithm
+  // for now, let's just use the 1us one which will trigger more interrupts
+
+  // now we need store this timeout information somewhere and allocate an id
+
+  timeout_t *new_timeout = malloc(sizeof(*new_timeout));
+  if (new_timeout == NULL) {
+    printf("[register_timer]: failed to allocate memory for new timeout");
+    return 0;
+  }
+  new_timeout->deadline = timeout_timestamp;
+  new_timeout->callback = callback;
+  new_timeout->data = data;
+
+  // now find a slot to store this, the index will be used as id
+
+  int slot_index = -1;
+  // TODO: This is why we need a hash table to keep track of the timeouts
+  // allocate id would be O(1) if we do that
+  for (int i = 0; i < clock.num_timeouts; i++) {
+    if (clock.timeouts[i] == NULL) {
+      slot_index = i;
+      break;
+    }
+  }
+
+  if (slot_index == -1) {
+    // need to reallocate memory
+    clock.num_timeouts++;
+    clock.timeouts =
+        realloc(clock.timeouts, clock.num_timeouts * sizeof(timeout_t *));
+    if (clock.timeouts == NULL) {
+      printf("[register_timer]: failed to reallocate memory for timeouts\n");
+      return 0;
+    }
+    slot_index = clock.num_timeouts - 1;
+  }
+
+  // now we have a slot to store the timeout, update the id
+
+  new_timeout->id = slot_index;
+
+  // now insert
+  clock.timeouts[slot_index] = new_timeout;
+
+  int success = pqueue_insert(clock.timeouts_queue, new_timeout);
+  if (success != 0) {
+    printf("[register_timer]: failed to insert\n");
+    // should free the memory here, but i am lazy
+    return 0; // failed to insert
+  }
+
+  // after we insert the timeout, we need to set the timer to the earliest
+  // timeout
+
+  // get next earliest timeout
+  timeout_t *earliest = pqueue_peek(clock.timeouts_queue);
+
+  if (earliest->id == new_timeout->id) {
+    // if we reach here, I am the earliest timeout, I set the timer
+
+    // if the delay is bigger than the max value of 16 bit timer, we have no
+    // choice just set the timer to the max value and recheck it when it goes
+    // off next time.
+    // we repeat this process until the delay is less than the max value which
+    // then we set the timer to that value
+
+    uint64_t timeout_us = delay;
+    if (timeout_us > UINT16_MAX) {
+      timeout_us = UINT16_MAX;
+    }
+
+    configure_timeout(clock.regs, MESON_TIMER_A, true, false,
+                      TIMEOUT_TIMEBASE_1_US, (uint16_t)timeout_us);
+  } else {
+    // not the earliest timeout, do nothing
+  }
+
+  return new_timeout->id;
 }
 
-int remove_timer(uint32_t id) { return CLOCK_R_FAIL; }
+int remove_timer(uint32_t id) {
+  // find the timeout with the id and remove it
+
+  if (clock.timeouts == NULL) {
+    printf("[remove_timer]: clock has been deallocated, nothing to remove\n");
+    return CLOCK_R_UINT;
+  }
+
+  // since we are using index as id, it is easy to find the timeout
+
+  if (id >= clock.num_timeouts) {
+    // definitely something wrong
+    printf("[remove_timer]: id: %u out of bounds\n", id);
+    return CLOCK_R_FAIL;
+  }
+
+  timeout_t *timeout_to_remove = clock.timeouts[id];
+
+  if (timeout_to_remove == NULL) {
+    // already removed
+    printf("[remove_timer]: removing an already removed timeout\n");
+    return CLOCK_R_FAIL;
+  }
+
+  // remove the timeout from the queue
+  int success = pqueue_remove(clock.timeouts_queue, timeout_to_remove);
+  if (success != 0) {
+    printf("[remove_timer]: failed to remove from queue\n");
+    return CLOCK_R_FAIL;
+  }
+
+  // free the timeout from the array
+  free(timeout_to_remove);
+
+  clock.timeouts[id] = NULL;
+
+  // may need to turn off the timer here as well if there are no more timeouts
+
+  return CLOCK_R_OK;
+}
 
 int timer_irq(void *data, seL4_Word irq, seL4_IRQHandler irq_handler) {
   /* Handle the IRQ */
