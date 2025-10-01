f9e193f5d09c840548523c7350ccd047d4c3dcb4
Morgan S <morgan.swaak@gmail.com>
2025-10-01 10:43:00 +1000

    Code to find best fit time base and calculate when a timer is due in respect to now

--- projects/aos/libclock/src/clock.c
+++ projects/aos/libclock/src/clock.c
@@ -17,10 +17,19 @@
  * to set registers and configure timeouts. */
 #include "device.h"
 
+struct delay {
+    uint16_t start_count;
+    timeout_timebase_t timer_base;
+}
+
+struct delay delay_to_16(uint64_t real_delay);
+
 static struct {
     volatile meson_timer_reg_t *regs;
     /* Add fields as you see necessary */
 } clock;
+
+
 
 int start_timer(unsigned char *timer_vaddr)
 {
@@ -34,9 +43,66 @@ int start_timer(unsigned char *timer_vaddr)
     return CLOCK_R_OK;
 }
 
+timestamp_t get_time(void) {
+    // TODO: check if timer_e hi is meant to be higher addresses
+    return timer_e_hi << 32 | timer_e;
+}
+
 uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data)
 {
-    return 0;
+    // add delay to current time
+    timestamp_t end_timestamp = delay + get_time();
+    // TODO: store expected end timestamp on the min-heap
+    uint32_t identifier = ...;
+    // TODO: check if head of min-heap
+
+    // if head start timer
+    timestamp_t = head;
+    uint64_t delay = head - get_time();
+    struct delay timer_data =  delay_to_16(delay);
+    // TODO, create timer
+    configure_timeout()
+    write_timeout()
+
+    // TODO: return int identifier for the timestamp
+    return identifier;
+}
+
+// converts a 64 bit delay into a 32 bit number (where lower 16 bits are..)
+struct delay delay_to_16(uint64_t real_delay) {
+    struct delay delay;
+    delay.start_count = 0;
+    // TODO: note, if more than the first 36 bits are set, it is impossible to represent as:
+    // 2^35 < 2^16 -1 * 10^6 < 2^36
+    // unless we make multiple delays for one delay?
+    // case only lower 16 bits are set (keep in microseconds)
+    if (real_delay >> 16 == 0) {
+        delay.start_count |= real_delay;
+        delay.timer_base = TIMEOUT_TIMEBASE_1_US;
+    }
+    uint64_t temp_dealy = real_delay / 10;
+    // case 10 microseconds
+    if (temp_dealy >> 16 == 0) {
+        delay.start_count |= temp_dealy;
+        delay.timer_base = TIMEOUT_TIMEBASE_10_US;
+    }
+    temp_dealy /= 10;
+    //case 100 microseconds
+    if (temp_dealy >> 16 == 0) {
+        delay.start_count |= temp_dealy;
+        delay.timer_base = TIMEOUT_TIMEBASE_100_US;
+    }
+    temp_dealy /= 10;
+    // case 1 milisecond
+    if (temp_dealy >> 16 == 0) {
+        delay.start_count |= temp_dealy;
+        delay.timer_base = TIMEOUT_TIMEBASE_1_MS;
+    // case it cant be represented so do longest delay possible?
+    } else {
+        delay.start_count = 0xFFFF;
+        delay.timer_base = TIMEOUT_TIMEBASE_1_MS;
+    }
+    return delay;
 }
 
 int remove_timer(uint32_t id)
