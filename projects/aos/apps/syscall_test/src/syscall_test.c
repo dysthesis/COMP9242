c9dd13c0db69d82e01ac080dbb04c01b2c46e488
Dysthesis <antheoraviel@protonmail.com>
2025-10-05 22:41:40 +1100

    Basic test for sos_open()

--- projects/aos/apps/syscall_test/src/syscall_test.c
+++ projects/aos/apps/syscall_test/src/syscall_test.c
@@ -1,4 +1,6 @@
 #include "unistd.h"
+#include <errno.h>
+#include <fcntl.h>
 #include <aos/sel4_zf_logif.h>
 #include <assert.h>
 #include <sel4/sel4.h>
@@ -12,6 +14,24 @@
 
 char test_str[] = "Basic test string for read/write";
 char small_buf[SMALL_BUF_SZ];
+
+static void test_sos_open(void) {
+  int fd_rd = sos_open("console", O_RDONLY);
+  assert(fd_rd >= 0);
+
+  int fd_wr = sos_open("console", O_WRONLY);
+  assert(fd_wr >= 0);
+  assert(fd_wr != fd_rd);
+
+  int fd_invalid = sos_open("not-a-device", O_RDONLY);
+  assert(fd_invalid == -ENODEV);
+
+  char long_name[MAX_IO_BUF + 1];
+  memset(long_name, 'a', sizeof long_name);
+  long_name[MAX_IO_BUF] = '\0';
+  int fd_long = sos_open(long_name, O_RDONLY);
+  assert(fd_long == -ENAMETOOLONG);
+}
 
 int test_buffers(int console_fd) {
   /* test a small string from the code segment */
@@ -46,6 +66,7 @@ int test_buffers(int console_fd) {
 
 int main(void) {
   ZF_LOGV("[syscall_test] Entered syscall testing app!\n");
+  test_sos_open();
   test_buffers(10);
 
   return 0;
